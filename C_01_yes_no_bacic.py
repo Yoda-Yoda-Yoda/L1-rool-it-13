fbe40ef511e0eb1e01ae0e4330ec764652644c84
Yoda-Yoda-Yoda <chambersl3255@masseyhigh.school.nz>
2025-02-05 12:05:39 +1300

    looping Yes/No checker

--- C_01_yes_no_bacic.py
+++ C_01_yes_no_bacic.py
@@ -1,10 +1,15 @@
-want_instructions = input("\ndo you want to read the instruction ").lower()
+while True:
+    want_instructions = input("\ndo you want to read the instruction ").lower()
 
-if want_instructions == "yes" or want_instructions == "y":
-    print("you said yes")
-elif want_instructions == "no" or want_instructions == "n":
-    print("you said no")
-else:
-    print("enter yes/no")
+    if want_instructions == "yes" or want_instructions == "y":
+        print("you said yes")
+        break
+    elif want_instructions == "no" or want_instructions == "n":
+        print("you said no")
+        break
+    else:
+        print("enter yes/no")
+        continue
 
 
+print("we dpne")
