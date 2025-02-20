5b8bbce03733290ddbed3e8f5b97b5faa8ee0fdc
Yoda-Yoda-Yoda <chambersl3255@masseyhigh.school.nz>
2025-02-21 11:26:20 +1300

    Working Single round

--- C_04_One_Round_v2.py
+++ C_04_One_Round_v2.py
@@ -19,6 +19,12 @@ def initial_points(which_player):
     return total, double
 
 
+def make_statement(statement, decoration):
+    """Adds emoji / additional characters to the start and end of headings"""
+
+    ends = decoration * 3
+    print(f" {ends}  {statement} {ends}")
+
 
 
 initial_user = initial_points("User")
@@ -54,7 +60,7 @@ else:
 
 while player_1_points < 13 and player_2_points < 13:
     print()
-    print("Press <enter> to continue this round\n")
+    input("Press <enter> to continue this round\n")
 
 
     player_1_roll = random.randint(1,6)
@@ -73,4 +79,31 @@ while player_1_points < 13 and player_2_points < 13:
 
     print(f"{first}: {player_1_points}    | {second} {player_2_points}")
 
-print("end of round")
+# end of round
+
+# associate player player points with either the user or the computer
+user_points = player_1_points
+comp_points = player_2_points
+
+
+# switch the user and computer points if the computer went first
+if first == "Computer":
+    user_points, comp_points = comp_points, user_points
+
+# work out who won...
+if user_points > comp_points:
+    winner = "user"
+else:
+    winner = "computer"
+
+round_feedback = f"The {winner} won."
+
+# double points if eligible
+if winner == "user" and double_user == "yes":
+    user_points = user_points * 2
+
+# output round results
+make_statement("Round Results", "=")
+print(f"User Points: {user_points}  |  Computer points: {comp_points} ")
+print(round_feedback)
+print()
